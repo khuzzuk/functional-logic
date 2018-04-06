005037f69d0cadb9801f3f1ad2377081fef06b0b
Adrian Drabik <adrian.drabik@silvermedia.pl>
2018-04-06 09:03:58 +0200

    refactor cleaning

--- src/main/java/pl/khuzzuk/functions/Validators.java
+++ src/main/java/pl/khuzzuk/functions/Validators.java
@@ -5,8 +5,10 @@ import java.util.function.Predicate;
 
 public class Validators
 {
-   private static final Predicate TRUE = __ -> true;
+   private static final Predicate TRUE = any -> true;
    private static final Predicate NULL_SAFE = Objects::nonNull;
+
+   private Validators() { }
 
    @SuppressWarnings("unchecked")
    public static <T> Predicate<T> alwaysTrue()
